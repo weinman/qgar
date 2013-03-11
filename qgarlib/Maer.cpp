b9649614f3a788aec92a71f50c689c755a5d993c
Chesnokov Yuriy <chesnokov.yuriy@gmail.com>
2013-03-11 15:04:52 +0400

    corrected MAER calculation bug
    original minimal area of enclosing rectangle (MAER) method implementation was not correct on different connected components elements
    method calculation was redundant processing all angles between consecutive convex hull points did not taking into account duplication
    min/max x,y coordinates estimation was unable to account for negative values

--- qgarlib/Maer.cpp
+++ qgarlib/Maer.cpp
@@ -18,8 +18,8 @@
  | Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.       |
  |                                                                     |
  | Contact Project Qgar for any information:                           |
- |   LORIA - équipe Qgar                                               |
- |   B.P. 239, 54506 Vandoeuvre-lès-Nancy Cedex, France                |
+ |   LORIA - Ð¹quipe Qgar                                               |
+ |   B.P. 239, 54506 Vandoeuvre-lÐ¸s-Nancy Cedex, France                |
  |   email: qgar-contact@loria.fr                                      |
  |   http://www.qgar.org/                                              |
  *---------------------------------------------------------------------*/
@@ -31,7 +31,7 @@
  *
  * See file Maer.h for the interface.
  *
- * @author <a href="mailto:qgar-develop@loria.fr?subject=Qgar fwd Gérald Masini">Gérald Masini</a>
+ * @author <a href="mailto:qgar-develop@loria.fr?subject=Qgar fwd GÐ¹rald Masini">GÐ¹rald Masini</a>
  * @date   May 13, 2004  11:30
  * @since  Qgar 2.1
  */
@@ -183,195 +183,95 @@ Maer::PRIVATEcomputeMaer(const std::list<DPoint>& aPtList)
       vertices_.push_back(hull.accessPivot());
       vertices_.push_back(hull.accessPivot());
       areaPx_ = 1.;
-
       return;
-    }
+    }  
 
   // COMPUTE THE DIFFERENT RECTANGLES AND SELECT THE MINIMAL ONE
-  // ===========================================================
+  // ===========================================================  
+
+  // Current minimum area
+  areaPx_ = Math::QG_DOUBLE_MAX;
 
   // List of the vertices of the hull
   const std::list<DPoint>& vList = hull.accessVertices();
+
+  // List of unique angles between 2 consecutive points of the hull
+  std::list<double> thetas;
 
   // 2 consecutive points of the hull
   std::list<DPoint>::const_iterator itPrev = vList.begin();
   std::list<DPoint>::const_iterator itCurr = itPrev;
   itCurr++;
 
-  // Current minimum area
-  areaPx_ = std::numeric_limits<double>::max();
-
   // Consider each consecutive pair of hull vertices
   // and compute the corresponding rectangle
   // WARNING: The last pair includes the last and first vertices
 
-  do
-    // DO WHILE BLOCK  WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+  do    
     {
-
-      // Coordinates of previous point
-      double xPrev = itPrev->x();
-      double yPrev = itPrev->y();
-
       // Direction of the vector joining previous and current points 
-      double theta = atan2(itCurr->y() - yPrev, itCurr->x() - xPrev);
-      if (theta < 0.)
-	{
-	   theta += Math::QG_2PI;
-	}
+      double theta = atan2(itCurr->y() - itPrev->y(), itCurr->x() - itPrev->x());
+      // Move the angle into the first quadrant with matlab mod implementation
+      theta = theta - floor(theta / Math::QG_PI_2) * Math::QG_PI_2;
 
-      // Sinus and cosinus 
-      double cosTheta = cos(theta);
-      double sinTheta = sin(theta);
+      // Check if similar angle was not processed before
+      std::list<double>::iterator itTheta = std::find_if(thetas.begin(), thetas.end(), [=](double value) -> bool { return qgEqRadian(theta, value); });
+      if (itTheta == thetas.end())
+        {
+          thetas.push_back(theta);
 
-      // Initializations
-      double minX = std::numeric_limits<double>::max();
-      double maxX = std::numeric_limits<double>::min();
-      double minY = std::numeric_limits<double>::max();
-      double maxY = std::numeric_limits<double>::min();
+          // Sinus and cosinus
+          double cosTheta = cos(-theta);
+          double sinTheta = sin(-theta);
+
+          // Initializations
+          double minX = Math::QG_DOUBLE_MAX;
+          double maxX = -Math::QG_DOUBLE_MAX;
+          double minY = Math::QG_DOUBLE_MAX;
+          double maxY = -Math::QG_DOUBLE_MAX;
       
-      // Minimum and maximum X and Y
-      for (std::list<DPoint>::const_iterator itV  = vList.begin() ;
-	   itV != vList.end() ;
-	   itV++)
-	{ 
-	  // Translation
-	  double xTmp = itV->x() - xPrev;
-	  double yTmp = itV->y() - yPrev;
+          // Minimum and maximum X and Y
+          for (std::list<DPoint>::const_iterator itV = vList.begin(); itV != vList.end(); ++itV)
+            { 
+              // Rotate the data through -theta
+              double x = itV->x() * cosTheta + itV->y() * (-sinTheta);
+              double y = itV->x() * sinTheta + itV->y() * cosTheta;	  
 	  
-	  // Rotation
-	  double x = ( xTmp * cosTheta) + (yTmp * sinTheta);
-	  double y = (-xTmp * sinTheta) + (yTmp * cosTheta);
-	  
-	  // Update coordinates min's and max's
-	  if (x < minX)
-	    {
-	      minX = x;
-	    }
-	  else if (x > maxX)
-	    {
-	      maxX = x;
-	    }
+	      // Update coordinates min's and max's
+	      if (x < minX)
+	        {
+	          minX = x;
+	        }
+	      else if (x > maxX)
+	        {
+	          maxX = x;
+	        }
 
-	  if (y < minY)
-	    {
-	      minY = y;
-	    }
-	  else if (y > maxY)
-	    {
-	      maxY = y;
-	    }
-	}
+	      if (y < minY)
+	        {
+	          minY = y;
+	        }
+	      else if (y > maxY)
+	        {
+	          maxY = y;
+	        }
+            }
       
-      // The four corners of the corresponding rectangle
+          // Current area
+          double currArea = ((maxX - minX) + 1) * ((maxY - minY) + 1);
 
-      double minXC = minX * cosTheta;
-      double minXS = minX * sinTheta;
-      double minYC = minY * cosTheta;
-      double minYS = minY * sinTheta;
-      double maxYC = maxY * cosTheta;
-      double maxYS = maxY * sinTheta;
-      double maxXC = maxX * cosTheta;
-      double maxXS = maxX * sinTheta;
+          if (currArea < areaPx_)
+            {
+              areaPx_ = currArea;
 
-      double xTopL = minXC - minYS + xPrev;
-      double yTopL = minXS + minYC + yPrev;
-      
-      double xTopR = minXC - maxYS + xPrev;
-      double yTopR = minXS + maxYC + yPrev;
-      
-      double xBotR = maxXC - maxYS + xPrev;
-      double yBotR = maxXS + maxYC + yPrev;
-      
-      double xBotL = maxXC - minYS + xPrev;
-      double yBotL = maxXS + minYC + yPrev;
-
-       // _______________________________________________________________
-       //
-       // WARNING: PARTICULAR CASES
-       // - the area is supposed to be expressed in pixels
-       // - the pixels defining the borders belong to the rectangle
-       // ==>
-       // - add 1 to the difference between X and Y coordinates
-       //   unless the difference is 0
-       // - if a couple of differences are both 0,
-       //   let one difference be 1
-
-       double dX1 = qgAbs(xTopL - xTopR);
-       double dY1 = qgAbs(yTopL - yTopR);
-       double dX2 = qgAbs(xTopR - xBotR);
-       double dY2 = qgAbs(yTopR - yBotR);
-
-       if (qgEq0(dX1))
-	 {
-	   dX1 = 0.;
-
-	   if (qgEq0(dY1))
-	     {
-	       dY1 = 1.;
-	     }
-	   else
-	     {
-	       dY1 += 1.;
-	     }
-	 }
-       else
-	 {
-	   dX1 += 1.;
-
-	   if (qgEq0(dY1))
-	     {
-	       dY1 = 0.;
-	     }
-	   else
-	     {
-	       dY1 += 1.;
-	     }
-	 }
-
-       if (qgEq0(dX2))
-	 {
-	   dX2 = 0.;
-
-	   if (qgEq0(dY2))
-	     {
-	       dY2 = 1.;
-	     }
-	   else
-	     {
-	       dY2 += 1.;
-	     }
-	 }
-       else
-	 {
-	   dX2 += 1.;
-
-	   if (qgEq0(dY2))
-	     {
-	       dY2 = 0.;
-	     }
-	   else
-	     {
-	       dY2 += 1.;
-	     }
-	 }
-
-       double currArea = hypot(dX1,dY1) * hypot(dX2,dY2);
-       // _______________________________________________________________    
-
-      // Update minimum area
-      if (currArea < areaPx_)
-	{
-	  // Current rectangle is the minimum one up to now
-	  areaPx_ = currArea;
-
-	  // Save new maer
-	  vertices_.clear();
-	  vertices_.push_back(DPoint(xTopL, yTopL));
-	  vertices_.push_back(DPoint(xTopR, yTopR));
-	  vertices_.push_back(DPoint(xBotR, yBotR));
-	  vertices_.push_back(DPoint(xBotL, yBotL));
-	}
+              vertices_.clear();          
+              // Unrotate coordinates
+              vertices_.push_back(DPoint(minX * cosTheta + minY * sinTheta, minX * (-sinTheta) + minY * cosTheta));
+              vertices_.push_back(DPoint(maxX * cosTheta + minY * sinTheta, maxX * (-sinTheta) + minY * cosTheta));
+              vertices_.push_back(DPoint(maxX * cosTheta + maxY * sinTheta, maxX * (-sinTheta) + maxY * cosTheta));
+              vertices_.push_back(DPoint(minX * cosTheta + maxY * sinTheta, minX * (-sinTheta) + maxY * cosTheta));
+            }
+        }      
 
       // Next vertices pair
       // WARNING: The last pair includes the last and first vertices
@@ -381,12 +281,8 @@ Maer::PRIVATEcomputeMaer(const std::list<DPoint>& aPtList)
 	{
 	  itCurr = vList.begin();
 	}
-
     }
-    // END OF DO WHILE BLOCK  WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
-
   while (itPrev != vList.begin());
-
 }
 
 // -------------------------------------------------------------------
